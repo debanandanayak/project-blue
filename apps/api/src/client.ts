6fb9fa6f5da92f08c07fb898527f67a7868c9892
Deba <rdebanandanayak@gmail.com>
2025-07-12 20:34:45 +0000

    setup prisma

---
+++ apps/api/src/client.ts
@@ -0,0 +1,10 @@
+import {PrismaClient} from "@prisma/client"
+
+interface CustomNodeJsGlobal extends Global{
+    prisma:PrismaClient
+}
+declare const global :CustomNodeJsGlobal
+
+const prisma = global.prisma || new PrismaClient()
+
+export default prisma
